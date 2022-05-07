19362b7cbdcd64d0272cabdc12e7de4874d64d4e
Dan Ponte <amigan@gmail.com>
2022-05-07 14:58:03 -0400

    Add flag for root

--- cmd/oghttpd/oghttpd.go
+++ cmd/oghttpd/oghttpd.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/ricomputermuseum/oghttpd/internal/httpd"
 )
 
 func main() {
-	h, err := httpd.NewHTTPd(":80", "./pub")
+	var wwwRoot string
+	flag.StringVar(&wwwRoot, "root", "./pub", "WWW root")
+	flag.Parse()
+
+	h, err := httpd.NewHTTPd(":80", wwwRoot)
 	if err != nil {
 		panic(err)
 	}
